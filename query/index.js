88095a67a0d7a09e480a1bd0d2cde325aad7609e
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-08-04 21:39:29 -0700

    Query service added

---
+++ query/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/posts', (request, response) => {});
+
+app.post('/events', (request, response) => {});
+
+app.listen(4002, () => {
+    console.log('Listening on 4002');
+});
